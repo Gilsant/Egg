f58fb01f651bbf112788c35c10e8b1b351dc104e
Jayson Steffens <jsteffens@hchb.com>
2023-05-29 20:12:09 -0500

    Testing Purposes

---
+++ src/firstproject/FirstProject.java
@@ -0,0 +1,21 @@
+package firstproject;
+
+import java.util.Scanner; // Este es el importe de la clase Scanner
+
+
+
+    public class FirstProject {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner leer1 = new Scanner(System.in);
+   
+        System.out.println("Cual es tu nombre?");
+        String nombre = leer1.nextLine();
+         System.out.println("Bienvenid@ " +nombre+ ", cuantos años tenes?");
+    Scanner leer2 = new Scanner(System.in);
+        int edad = leer2.nextInt();
+        System.out.println("Wow! "+edad+ " años");
+    
+    }
+
+}
